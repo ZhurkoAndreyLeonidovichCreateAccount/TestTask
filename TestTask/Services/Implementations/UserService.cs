41691a51399a7fdae4440d648034c73dc876e2ae
Andrei Zhurko <andrey675246zhurko@mail.ru>
2024-02-17 15:04:41 +0300

    Add project files.

---
+++ TestTask/Services/Implementations/UserService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+using TestTask.Data;
+using TestTask.Enums;
+
+namespace TestTask.Services.Implementations
+{
+    public class UserService : IUserService
+    {
+        private readonly ApplicationDbContext _context;
+        public UserService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<User> GetUser()
+        {
+            return await _context.Users.OrderByDescending(x => x.Orders.Count).FirstOrDefaultAsync() ?? new User();
+        }
+
+        public async Task<List<User>> GetUsers()
+        {
+           return await _context.Users.Where(u => u.Status == UserStatus.Inactive).ToListAsync();
+        }
+    }
+}
